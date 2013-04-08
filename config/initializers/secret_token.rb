76edad339c9416be7fe16f1321fe0dd3cd39389a
Paul Daniel <paul@new-host-3.home>
2013-04-07 21:27:12 -0700

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jaffr::Application.config.secret_token = '20abd3da763b22ebfa185693f01c7a0e25145624c558c5ac5975d6887c4cfde0e737dd434f5cb89cea6e556694e66e5827e8043fdb1783a3e2bb79115c7cfaea'
